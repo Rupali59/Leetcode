8e6fd74e040b5793b35ed6c3f0b83482c7606fb4
rupali.bhatnagar <rupali.bhatnagar@servicenow.com>
2022-09-27 21:43:16 +0530

    53: Kanades algorithm

--- Array/53. Maximum Subarray.js
+++ Array/53. Maximum Subarray.js
@@ -3,36 +3,41 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    if (nums.length == 1)
-        return nums[0];
+    /**
+     * Kanade's algorithm
+     * local_maximum at index i is the maximum of A[i] and the sum of A[i] and local_maximum at index i-1.
+     * local_max(i) = max(A[i], local_max(i-1)+A[i])
+     */
+     var local_max= [];
+     local_max[0] = nums[0];
+     var global_max = local_max[0];
 
-    var maxVal = nums[0];
-
-    var calcArray = nums.map(x => {
-        maxVal = Math.max(maxVal, x);
-        return x;
-    });
-
-    for (var i = 2; i <= nums.length; ++i) {
-        j = i-1;
-        for (k = 0; k < nums.length - i + 1; ++k) {
-            calcArray[k] = calcArray[k] + nums[k + j];
-            maxVal = Math.max(maxVal, calcArray[k]);
-        }
-        calcArray.pop();
-    }
-    return maxVal;
+     for(var i=1; i<nums.length; ++i){
+        local_max[i] = Math.max(nums[i], nums[i]+local_max[i-1]);
+        global_max = Math.max(local_max[i], global_max);
+     }
+     return global_max;
 };
+
+
+var localMaximum = function(A, local_max, i){
+    if(i == 0)
+        return A[0];
+    
+    
+    
+    return local_max[i];
+}
 
 
 var testcases = [
     [-2, 1, -3, 4, -1, 2, 1, -5, 4],
     [1],
     [5, 4, -1, 7, 8],
-    [-2,-1]
+    [-2, -1]
 ]
 var testcaseAnswers = [
-    6, 1, 23,-1
+    6, 1, 23, -1
 ];
 
 testcases.map((testcase, index) => {
