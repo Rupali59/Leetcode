6705edec7521d3aaaa5d98f304d0d9054fc6499d
rupali.bhatnagar <rupali.bhatnagar@servicenow.com>
2022-09-27 15:01:44 +0530

    217: add hash based soln

--- 217. Contains Duplicate.js
+++ 217. Contains Duplicate.js
@@ -5,12 +5,29 @@
  */
 
 var containsDuplicate = function(nums) {
-    var hash = {};
-    nums.map(digit => {
-        if (!hash[digit]) {
-            hash[digit] = 1;
+    var hash = new Map();
+    i = 0;
+    while (i < nums.length) {
+        if (!hash.has(nums[i])) {
+            hash.set(nums[i], 1);
         } else
-            return false;
-    });
-    return true;
+            return true;
+        i++;
+    }
+    return false;
 };
+
+
+var testcases = [
+    [1, 2, 3, 1],
+    [1, 2, 3, 4],
+    [1, 1, 1, 3, 3, 4, 3, 2, 4, 2],
+]
+var testcaseAnswers = [true, false, true];
+testcases.map((testcase, index) => {
+    var op = containsDuplicate(testcase);
+    if (op != testcaseAnswers[index])
+        console.log("Error: " + testcase + " Output: " + op + " Expected: " + testcaseAnswers[index]);
+    // else
+    //     console.log("Success: " + testcase + " Output: " + op + " Expected: " + testcaseAnswers[index]);
+})
