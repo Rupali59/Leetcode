5ed812df7ec24f628a4856208f0265cfaebc1f3a
Rupali Bhatnagar <rupalibhatnagar0509@gmail.com>
2025-08-21 19:33:42 +0530

    Time: 7 ms (55.80%) | Memory: 19.3 MB (86.72%) - LeetSync

--- Top150/169-majority-element/majority-element.py
+++ Top150/169-majority-element/majority-element.py
@@ -3,8 +3,15 @@ from collections import Counter
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        characterCounts = Counter(nums)
-        numElements = len(nums)
-        for i in characterCounts:
-            if characterCounts[i] > (numElements / 2):
-                return i
+        # Boyer-Moore Majority vote algorithm
+        candidate = None
+        count = 0
+        for i in range(len(nums)):
+            if(count == 0):
+                candidate = nums[i]
+            if(nums[i] == candidate):
+                count+=1
+            else:
+                count-=1
+
+        return candidate
