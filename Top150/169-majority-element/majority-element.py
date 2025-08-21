00e475b5b78dbad7512ad746d87a0795de58e710
Rupali Bhatnagar <rupalibhatnagar0509@gmail.com>
2025-08-21 21:38:06 +0530

    Time: 7 ms (55.80%) | Memory: 19.4 MB (18.93%) - LeetSync

--- Top150/169-majority-element/majority-element.py
+++ Top150/169-majority-element/majority-element.py
@@ -6,10 +6,10 @@ class Solution:
         # Boyer-Moore Majority vote algorithm
         candidate = None
         count = 0
-        for i in range(len(nums)):
+        for num in nums:
             if(count == 0):
-                candidate = nums[i]
-            if(nums[i] == candidate):
+                candidate = num
+            if(num == candidate):
                 count+=1
             else:
                 count-=1
